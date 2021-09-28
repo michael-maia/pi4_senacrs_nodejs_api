ad2e8f18f5dc795a0dfe9e547692b244bf1ca2f4
michael-maia <michaelmdasilva@yahoo.com.br>
2021-09-28 00:33:12 -0300

    Team Routes criado

---
+++ myapp/routes/team_routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const teamController = require('../controller/team_controller')
+
+router.get('/', teamController.showList)
+router.get('/search', teamController.search)
+router.get('/:id', teamController.findById)
+router.post('/', teamController.create)
+router.put('/:id', teamController.update)
+router.delete('/:id', teamController.delete)
+
+module.exports = router;
