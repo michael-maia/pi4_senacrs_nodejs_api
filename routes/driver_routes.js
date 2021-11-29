39c8095cf0f0175738009dc9a37b1182accb1717
Michael Maia <32021280+michael-maia@users.noreply.github.com>
2021-11-29 00:19:35 -0300

    Retirando userValidation() para testes c/ token

--- routes/driver_routes.js
+++ routes/driver_routes.js
@@ -7,7 +7,8 @@ router.get('/', driverController.showList);
 //router.get('/search', driverController.search);
 router.get('/showTeams', driverController.showTeams);
 router.get('/:id', driverController.findById);
-router.post('/', userController.tokenValidation, userController.isAdmin, driverController.create);
+//router.post('/', userController.tokenValidation, userController.isAdmin, driverController.create);
+router.post('/', driverController.create);
 router.put('/:id', userController.tokenValidation, userController.isAdmin, driverController.update);
 router.delete('/:id', userController.tokenValidation, userController.isAdmin, driverController.delete);
 
