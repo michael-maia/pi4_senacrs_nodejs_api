c1f0fe61507da46af26bc88a1af958c5051b0f19
Michael Maia <32021280+michael-maia@users.noreply.github.com>
2021-12-06 22:06:22 -0300

    teste

--- routes/team_routes.js
+++ routes/team_routes.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const teamController = require('../controllers/team_controller');
-//const userController = require('../controllers/user_controller');
+const userController = require('../controllers/user_controller');
 
 router.get('/', teamController.showList);
 //router.get('/search', teamController.search)
 router.get('/:id', teamController.findById);
-/* router.post('/', userController.tokenValidation, userController.isAdmin, teamController.create);
+router.post('/', userController.tokenValidation, userController.isAdmin, teamController.create);
 router.put('/:id', userController.tokenValidation, userController.isAdmin, teamController.update);
-router.delete('/:id', userController.tokenValidation, userController.isAdmin, teamController.delete); */
-router.post('/', teamController.create);
-router.put('/:id', teamController.update);
-router.delete('/:id', teamController.delete);
+router.delete('/:id', userController.tokenValidation, userController.isAdmin, teamController.delete);
 
 module.exports = router;
